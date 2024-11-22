c39e538e61020ac81657f6ecf5f8c96a917d6845
DoubleStyx <79298541+DoubleStyx@users.noreply.github.com>
2024-11-21 22:04:03 -0600

    add jobsystem

--- SyncraEngine/JobSystem.cs
+++ SyncraEngine/JobSystem.cs
@@ -2,14 +2,69 @@ namespace SyncraEngine;
 
 public sealed class JobSystem
 {
-    public List<ISystem> Systems = [];
-    public List<Task> Tasks = [];
+    private List<ISystem> Systems { get; } = new();
 
-    public void OnSystemUpdated()
+    public async Task Execute(Scene scene)
     {
-        // check systems for dependency resolution
+        var dependencyGraph = BuildDependencyGraph();
 
-        // job scheduler basically just tries to execute the DAG as fast as possible
-        // and then start over again
+        var sortedSystems = TopologicalSort(dependencyGraph);
+
+        var tasks = sortedSystems.Select(batch => Task.WhenAll(batch.Select(system => Task.Run(() => system.Update(scene))))).ToList();
+
+        await Task.WhenAll(tasks);
+    }
+
+    private Dictionary<ISystem, List<ISystem>> BuildDependencyGraph()
+    {
+        var graph = new Dictionary<ISystem, List<ISystem>>();
+
+        foreach (var system in Systems)
+        {
+            graph[system] = new List<ISystem>();
+            foreach (var dependency in system.Dependencies.Select(dependencyType => Systems.FirstOrDefault(static s => false)).OfType<ISystem>())
+            {
+                graph[system].Add(dependency);
+            }
+        }
+
+        return graph;
+    }
+
+    private static IEnumerable<List<ISystem>> TopologicalSort(Dictionary<ISystem, List<ISystem>> graph)
+    {
+        var inDegree = new Dictionary<ISystem, int>();
+        foreach (var system in graph)
+        {
+            inDegree[system.Key] = 0;
+        }
+
+        foreach (var dependency in from edges in graph.Values from dependency in edges where inDegree.ContainsKey(dependency) select dependency)
+        {
+            inDegree[dependency]++;
+        }
+
+        var zeroInDegree = new Queue<ISystem>(inDegree.Where(static kvp => kvp.Value == 0).Select(static kvp => kvp.Key));
+        var sortedBatches = new List<List<ISystem>>();
+
+        while (zeroInDegree.Count != 0)
+        {
+            var batch = new List<ISystem>();
+            foreach (var system in zeroInDegree.ToList())
+            {
+                batch.Add(system);
+                zeroInDegree.Dequeue();
+
+                foreach (var dependent in graph[system])
+                {
+                    inDegree[dependent]--;
+                    if (inDegree[dependent] == 0) zeroInDegree.Enqueue(dependent);
+                }
+            }
+
+            sortedBatches.Add(batch);
+        }
+
+        return sortedBatches;
     }
 }
