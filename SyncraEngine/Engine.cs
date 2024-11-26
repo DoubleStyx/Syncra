ea07ae9fc9c2c3bc1caeaca8c002b97748a6e1c1
DoubleStyx <79298541+DoubleStyx@users.noreply.github.com>
2024-11-26 02:48:03 -0600

    consolidate scenes into world

--- SyncraEngine/Engine.cs
+++ SyncraEngine/Engine.cs
@@ -2,8 +2,6 @@ namespace Syncra.SyncraEngine;
 
 public sealed class Engine
 {
-    public Guid Guid = new();
-    public string Name = "";
     public World? CurrentWorld = null;
     public List<World> Worlds = [];
 
--- SyncraEngine/Entity.cs
+++ SyncraEngine/Entity.cs
@@ -3,27 +3,27 @@ namespace Syncra.SyncraEngine;
 public struct Entity
 {
     internal Guid Guid;
-    private readonly Scene _scene;
+    private readonly World _world;
 
-    internal Entity(Scene scene)
+    internal Entity(World world)
     {
         Guid = Guid.NewGuid();
-        _scene = scene;
+        _world = world;
     }
 
     public readonly void AddComponent<T>() where T : Component, new()
     {
-        _scene.AddComponent<T>(this);
+        _world.AddComponent<T>(this);
     }
 
     public readonly T? TryGetComponent<T>() where T : Component
     {
-        _scene.TryGetComponent<T>(this, out var component);
+        _world.TryGetComponent<T>(this, out var component);
         return component;
     }
 
     public readonly void DestroyComponent<T>() where T : Component
     {
-        _scene.DestroyComponent<T>(this);
+        _world.DestroyComponent<T>(this);
     }
 }
