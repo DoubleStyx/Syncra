0f6f5a67e4443339b2ac6d46b5a5a07362f71ac7
DoubleStyx <79298541+DoubleStyx@users.noreply.github.com>
2024-11-29 03:02:41 -0600

    Update main.c

--- SyncraOS/src/main.c
+++ SyncraOS/src/main.c
@@ -23,4 +23,10 @@ int main() {
     glfwTerminate();
 
     return 0;
+
+    // SyncraOS is the main driver and broker for Syncra. It drives the Vulkan renderer
+    // and window/OpenXR context. It manages the lifecycles of various Syncra apps.
+    // Each app is a separate C# process that may or may not be sandboxed.
+    // You always start off in a local space, which is another C# process
+    // with privileged permissions. This is essentially your desktop environment.
 }
