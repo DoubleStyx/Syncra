67be7d156425f6903d43a20ebd83103ce2e9c82d
DoubleStyx <79298541+DoubleStyx@users.noreply.github.com>
2024-11-28 21:33:29 -0600

    cmake

--- SyncraEngine/main.c
+++ SyncraEngine/main.c
@@ -3,10 +3,7 @@
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
-#include <iostream>
+#include <cglm/mat4.h>
 
 int main() {
     glfwInit();
@@ -16,12 +13,6 @@ int main() {
 
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-    std::cout << extensionCount << " extensions supported\n";
-
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
 
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
