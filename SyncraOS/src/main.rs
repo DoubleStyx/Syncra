16f0b15b91130f4dab3780fa1b46a6c80aca231f
DoubleStyx <79298541+DoubleStyx@users.noreply.github.com>
2024-12-01 16:09:23 -0600

    use ash

--- SyncraOS/src/main.rs
+++ SyncraOS/src/main.rs
@@ -1,83 +1,237 @@
-use std::ffi::{c_int, CString};
-use std::ptr;
+use std::ffi;
+use std::ffi::c_char;
+use ash::{vk as vk, Device};
+use ash::{Entry, Instance};
+use ash::ext::debug_utils;
+use ash::khr::{surface, swapchain};
+use glfw::{Action, Context, Key};
+use raw_window_handle::{HasDisplayHandle, HasWindowHandle};
 
-#[allow(warnings)]
-pub mod vulkan {
-    include!(concat!(env!("OUT_DIR"), "/bindings/vulkan.rs"));
-}
-#[allow(warnings)]
-pub mod glfw {
-    include!(concat!(env!("OUT_DIR"), "/bindings/glfw.rs"));
-}
-#[allow(warnings)]
-pub mod cglm {
-    include!(concat!(env!("OUT_DIR"), "/bindings/cglm.rs"));
-}
-#[allow(warnings)]
-pub mod openxr {
-    include!(concat!(env!("OUT_DIR"), "/bindings/openxr.rs"));
-}
+fn main() {
+    let entry = Entry::linked();
 
+    let app_name = unsafe { ffi::CStr::from_bytes_with_nul_unchecked(b"Syncra\0") };
 
-pub fn main() {
-    let app_name = CString::new("Syncra").expect("Failed to create CString");
-    let engine_name = CString::new("SyncraOS").expect("Failed to create CString");
+    let appinfo = vk::ApplicationInfo::default()
+        .application_name(app_name)
+        .application_version(0)
+        .engine_name(app_name)
+        .engine_version(0)
+        .api_version(vk::make_api_version(0, 1, 0, 0));
 
-    let app_info = vulkan::VkApplicationInfo {
-        sType: vulkan::VkStructureType_VK_STRUCTURE_TYPE_APPLICATION_INFO,
-        pNext: ptr::null(),
-        pApplicationName: app_name.as_ptr(),
-        applicationVersion: 0,
-        pEngineName: engine_name.as_ptr(),
-        engineVersion: 0,
-        apiVersion: 0,
-    };
+    let layer_names = [unsafe {
+        ffi::CStr::from_bytes_with_nul_unchecked(
+            b"VK_LAYER_KHRONOS_validation\0",
+        )
+    }];
 
-    let create_info = vulkan::VkInstanceCreateInfo {
-        sType: vulkan::VkStructureType_VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
-        pNext: ptr::null(),
-        flags: 0,
-        pApplicationInfo: &app_info,
-        enabledLayerCount: 0,
-        ppEnabledLayerNames: ptr::null(),
-        enabledExtensionCount: 0,
-        ppEnabledExtensionNames: ptr::null(), 
-    };
+    let layers_names_raw: Vec<*const c_char> = layer_names
+        .iter()
+        .map(|raw_name| raw_name.as_ptr())
+        .collect();
 
+    let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
     
-    unsafe {
-        if glfw::glfwInit() == glfw::GLFW_FALSE as i32 {
-            eprintln!("Failed to initialize GLFW");
-            return;
-        }
+    let window_width = 800;
+    let window_height = 600;
+
+    let (mut window, events) = glfw.create_window(window_width, window_height, "Hello this is window", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+
+    window.set_key_polling(true);
+    window.make_current();
+
+    println!(
+        "Display Handle: {:?}, Window Handle: {:?}",
+        window.display_handle().unwrap().as_raw(),
+        window.window_handle().unwrap().as_raw()
+    );
+
+    let mut extension_names =
+        ash_window::enumerate_required_extensions(window.display_handle().unwrap().as_raw())
+            .unwrap()
+            .to_vec();
+    extension_names.push(debug_utils::NAME.as_ptr());
+
+    for ext in &extension_names {
+        println!("Extension: {:?}", unsafe { ffi::CStr::from_ptr(*ext) });
     }
 
-    unsafe { glfw::glfwWindowHint(glfw::GLFW_CLIENT_API as c_int, glfw::GLFW_NO_API as c_int); }
-    unsafe { glfw::glfwWindowHint(glfw::GLFW_RESIZABLE as c_int, glfw::GLFW_FALSE as c_int); }
-    
-    let title = CString::new("Vulkan").expect("Failed to create CString");
+    let create_flags = if cfg!(any(target_os = "macos", target_os = "ios")) {
+        vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+    } else {
+        vk::InstanceCreateFlags::default()
+    };
 
-    let window =
-        unsafe {
-            glfw::glfwCreateWindow(
-                800,
-                600,
-                title.as_ptr(),
-                ptr::null_mut(),
-                ptr::null_mut(),
-            )
-        };
+    let create_info = vk::InstanceCreateInfo::default()
+        .application_info(&appinfo)
+        .enabled_layer_names(&layers_names_raw)
+        .enabled_extension_names(&extension_names)
+        .flags(create_flags);
 
-    unsafe {
-        while glfw::glfwWindowShouldClose(window) == glfw::GLFW_FALSE as i32 {
-            glfw::glfwPollEvents();
-        }
+    let instance: Instance = unsafe {
+        entry
+            .create_instance(&create_info, None)
+    }
+        .expect("Instance creation error");
+
+
+    let surface = unsafe {
+        ash_window::create_surface(
+            &entry,
+            &instance,
+            window.display_handle().unwrap().as_raw(),
+            window.window_handle().unwrap().as_raw(),
+            None,
+        )
+    }
+        .unwrap();
+
+    let pdevices = unsafe {
+        instance
+            .enumerate_physical_devices()
+    }
+        .expect("Physical device error");
+
+    let surface_loader = surface::Instance::new(&entry, &instance);
+
+    let (pdevice, queue_family_index) = pdevices
+        .iter()
+        .find_map(|pdevice| {
+            unsafe { instance
+                .get_physical_device_queue_family_properties(*pdevice) }
+                .iter()
+                .enumerate()
+                .find_map(|(index, info)| {
+                    let supports_graphic_and_surface =
+                        info.queue_flags.contains(vk::QueueFlags::GRAPHICS)
+                            && unsafe { surface_loader
+                            .get_physical_device_surface_support(
+                                *pdevice,
+                                index as u32,
+                                surface,
+                            )}
+                            .unwrap();
+                    if supports_graphic_and_surface {
+                        Some((*pdevice, index))
+                    } else {
+                        None
+                    }
+                })
+        })
+        .expect("Couldn't find suitable device.");
+
+    let priorities = [1.0];
+
+    let queue_info = vk::DeviceQueueCreateInfo::default()
+        .queue_family_index(queue_family_index as u32)
+        .queue_priorities(&priorities);
+
+    let features = vk::PhysicalDeviceFeatures {
+        shader_clip_distance: 1,
+        ..Default::default()
+    };
+
+    let device_extension_names_raw = [
+        swapchain::NAME.as_ptr(),
+        #[cfg(any(target_os = "macos", target_os = "ios"))]
+        ash::khr::portability_subset::NAME.as_ptr(),
+    ];
+
+    let device_create_info = vk::DeviceCreateInfo::default()
+        .queue_create_infos(std::slice::from_ref(&queue_info))
+        .enabled_extension_names(&device_extension_names_raw)
+        .enabled_features(&features);
+
+    let device: Device = unsafe {
+        instance
+            .create_device(pdevice, &device_create_info, None)
+    }
+        .unwrap();
+
+
+    let swapchain_loader = swapchain::Device::new(&instance, &device);
+
+    let surface_capabilities = unsafe {
+        surface_loader
+            .get_physical_device_surface_capabilities(pdevice, surface)
+    }
+        .unwrap();
+
+    let desired_image_count = surface_capabilities.min_image_count + 1;
+
+    let surface_format = unsafe {
+        surface_loader
+            .get_physical_device_surface_formats(pdevice, surface)
+    }
+        .unwrap()[0];
+
+    let surface_resolution = match surface_capabilities.current_extent.width {
+        u32::MAX => vk::Extent2D {
+            width: window_width,
+            height: window_height,
+        },
+        _ => surface_capabilities.current_extent,
+    };
+
+    let pre_transform = if surface_capabilities
+        .supported_transforms
+        .contains(vk::SurfaceTransformFlagsKHR::IDENTITY)
+    {
+        vk::SurfaceTransformFlagsKHR::IDENTITY
+    } else {
+        surface_capabilities.current_transform
+    };
+
+    let present_modes = unsafe {
+        surface_loader
+            .get_physical_device_surface_present_modes(pdevice, surface)
+    }
+        .unwrap();
+
+    let present_mode = present_modes
+        .iter()
+        .cloned()
+        .find(|&mode| mode == vk::PresentModeKHR::MAILBOX)
+        .unwrap_or(vk::PresentModeKHR::FIFO);
+
+    let swapchain_create_info = vk::SwapchainCreateInfoKHR::default()
+        .surface(surface)
+        .min_image_count(desired_image_count)
+        .image_color_space(surface_format.color_space)
+        .image_format(surface_format.format)
+        .image_extent(surface_resolution)
+        .image_usage(vk::ImageUsageFlags::COLOR_ATTACHMENT)
+        .image_sharing_mode(vk::SharingMode::EXCLUSIVE)
+        .pre_transform(pre_transform)
+        .composite_alpha(vk::CompositeAlphaFlagsKHR::OPAQUE)
+        .present_mode(present_mode)
+        .clipped(true)
+        .image_array_layers(1);
+
+    let swapchain = unsafe {
+        swapchain_loader
+            .create_swapchain(&swapchain_create_info, None)
+    }
+        .unwrap();
+
+    let pool_create_info = vk::CommandPoolCreateInfo::default()
+        .flags(vk::CommandPoolCreateFlags::RESET_COMMAND_BUFFER)
+        .queue_family_index(queue_family_index as u32);
+
+    let pool = unsafe { device.create_command_pool(&pool_create_info, None) }.unwrap();
+
+    while !window.should_close() {
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            match event {
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    window.set_should_close(true)
+                }
+                _ => {}
+            }        }
     }
 
-    unsafe { glfw::glfwDestroyWindow(window); }
-
-    unsafe { glfw::glfwTerminate(); }
-    
     // SyncraOS is the main driver and broker for Syncra. It drives the Vulkan renderer
     // and window/OpenXR context. It manages the lifecycles of various Syncra apps.
     // Each app is a separate C# process that may or may not be sandboxed.
