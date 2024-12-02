4636fb208e67a1fe4b9deca87cc1b99eee3656f1
DoubleStyx <79298541+DoubleStyx@users.noreply.github.com>
2024-12-02 01:12:51 -0600

    add vulkan initialization

--- SyncraOS/src/main.rs
+++ SyncraOS/src/main.rs
@@ -1,9 +1,12 @@
 use std::ffi;
 use std::ffi::c_char;
+use std::io::Cursor;
+use std::mem::offset_of;
 use ash::{vk as vk, Device};
 use ash::{Entry, Instance};
 use ash::ext::debug_utils;
 use ash::khr::{surface, swapchain};
+use ash::util::read_spv;
 use glfw::{Action, Context, Key};
 use raw_window_handle::{HasDisplayHandle, HasWindowHandle};
 
@@ -210,6 +213,250 @@ fn main() {
 
     let pool = unsafe { device.create_command_pool(&pool_create_info, None) }.unwrap();
 
+    let command_buffer_allocate_info = vk::CommandBufferAllocateInfo::default()
+        .command_buffer_count(2)
+        .command_pool(pool)
+        .level(vk::CommandBufferLevel::PRIMARY);
+
+    let command_buffers = unsafe {
+        device
+            .allocate_command_buffers(&command_buffer_allocate_info)
+    }
+        .unwrap();
+
+    let setup_command_buffer = command_buffers[0];
+    let draw_command_buffer = command_buffers[1];
+
+    let semaphore_create_info = vk::SemaphoreCreateInfo::default();
+
+    let present_complete_semaphore = unsafe {
+        device
+            .create_semaphore(&semaphore_create_info, None)
+    }
+        .unwrap();
+    let rendering_complete_semaphore = unsafe {
+        device
+            .create_semaphore(&semaphore_create_info, None)
+    }
+        .unwrap();
+
+    let viewports = [vk::Viewport {
+        x: 0.0,
+        y: 0.0,
+        width: surface_resolution.width as f32,
+        height: surface_resolution.height as f32,
+        min_depth: 0.0,
+        max_depth: 1.0,
+    }];
+    let scissors = [surface_resolution.into()];
+
+    let viewport_state_info = vk::PipelineViewportStateCreateInfo::default()
+        .scissors(&scissors)
+        .viewports(&viewports);
+
+    let mut vertex_spv_file =
+        Cursor::new(&include_bytes!("../../shader/triangle/vert.spv")[..]);
+    let mut frag_spv_file = Cursor::new(&include_bytes!("../../shader/triangle/frag.spv")[..]);
+
+    let vertex_code =
+        read_spv(&mut vertex_spv_file).expect("Failed to read vertex shader spv file");
+    let vertex_shader_info = vk::ShaderModuleCreateInfo::default().code(&vertex_code);
+
+    let frag_code =
+        read_spv(&mut frag_spv_file).expect("Failed to read fragment shader spv file");
+    let frag_shader_info = vk::ShaderModuleCreateInfo::default().code(&frag_code);
+
+    let vertex_shader_module = unsafe {
+        device
+            .create_shader_module(&vertex_shader_info, None)
+    }
+        .expect("Vertex shader module error");
+
+    let fragment_shader_module = unsafe {
+        device
+            .create_shader_module(&frag_shader_info, None)
+    }
+        .expect("Fragment shader module error");
+
+    let shader_entry_name = unsafe { ffi::CStr::from_bytes_with_nul_unchecked(b"main\0") };
+
+    let shader_stage_create_infos = [
+        vk::PipelineShaderStageCreateInfo {
+            module: vertex_shader_module,
+            p_name: shader_entry_name.as_ptr(),
+            stage: vk::ShaderStageFlags::VERTEX,
+            ..Default::default()
+        },
+        vk::PipelineShaderStageCreateInfo {
+            s_type: vk::StructureType::PIPELINE_SHADER_STAGE_CREATE_INFO,
+            module: fragment_shader_module,
+            p_name: shader_entry_name.as_ptr(),
+            stage: vk::ShaderStageFlags::FRAGMENT,
+            ..Default::default()
+        },
+    ];
+
+    let vertex_input_attribute_descriptions = [
+        vk::VertexInputAttributeDescription {
+            location: 0,
+            binding: 0,
+            format: vk::Format::R32G32B32A32_SFLOAT,
+            offset: offset_of!(Vertex, pos) as u32,
+        },
+        vk::VertexInputAttributeDescription {
+            location: 1,
+            binding: 0,
+            format: vk::Format::R32G32B32A32_SFLOAT,
+            offset: offset_of!(Vertex, color) as u32,
+        },
+    ];
+
+    let vertex_input_binding_descriptions = [vk::VertexInputBindingDescription {
+        binding: 0,
+        stride: size_of::<Vertex>() as u32,
+        input_rate: vk::VertexInputRate::VERTEX,
+    }];
+
+    let vertex_input_state_info = vk::PipelineVertexInputStateCreateInfo::default()
+        .vertex_attribute_descriptions(&vertex_input_attribute_descriptions)
+        .vertex_binding_descriptions(&vertex_input_binding_descriptions);
+
+    let vertex_input_assembly_state_info = vk::PipelineInputAssemblyStateCreateInfo {
+        topology: vk::PrimitiveTopology::TRIANGLE_LIST,
+        ..Default::default()
+    };
+
+    let rasterization_info = vk::PipelineRasterizationStateCreateInfo {
+        front_face: vk::FrontFace::COUNTER_CLOCKWISE,
+        line_width: 1.0,
+        polygon_mode: vk::PolygonMode::FILL,
+        ..Default::default()
+    };
+
+    let multisample_state_info = vk::PipelineMultisampleStateCreateInfo {
+        rasterization_samples: vk::SampleCountFlags::TYPE_1,
+        ..Default::default()
+    };
+
+    let noop_stencil_state = vk::StencilOpState {
+        fail_op: vk::StencilOp::KEEP,
+        pass_op: vk::StencilOp::KEEP,
+        depth_fail_op: vk::StencilOp::KEEP,
+        compare_op: vk::CompareOp::ALWAYS,
+        ..Default::default()
+    };
+
+    let depth_state_info = vk::PipelineDepthStencilStateCreateInfo {
+        depth_test_enable: 1,
+        depth_write_enable: 1,
+        depth_compare_op: vk::CompareOp::LESS_OR_EQUAL,
+        front: noop_stencil_state,
+        back: noop_stencil_state,
+        max_depth_bounds: 1.0,
+        ..Default::default()
+    };
+
+    let color_blend_attachment_states = [vk::PipelineColorBlendAttachmentState {
+        blend_enable: 0,
+        src_color_blend_factor: vk::BlendFactor::SRC_COLOR,
+        dst_color_blend_factor: vk::BlendFactor::ONE_MINUS_DST_COLOR,
+        color_blend_op: vk::BlendOp::ADD,
+        src_alpha_blend_factor: vk::BlendFactor::ZERO,
+        dst_alpha_blend_factor: vk::BlendFactor::ZERO,
+        alpha_blend_op: vk::BlendOp::ADD,
+        color_write_mask: vk::ColorComponentFlags::RGBA,
+    }];
+
+    let color_blend_state = vk::PipelineColorBlendStateCreateInfo::default()
+        .logic_op(vk::LogicOp::CLEAR)
+        .attachments(&color_blend_attachment_states);
+
+    let dynamic_state = [vk::DynamicState::VIEWPORT, vk::DynamicState::SCISSOR];
+    let dynamic_state_info =
+        vk::PipelineDynamicStateCreateInfo::default().dynamic_states(&dynamic_state);
+
+    let layout_create_info = vk::PipelineLayoutCreateInfo::default();
+
+    let pipeline_layout = unsafe {
+        device
+            .create_pipeline_layout(&layout_create_info, None)
+    }
+        .unwrap();
+
+    let dependencies = [vk::SubpassDependency {
+        src_subpass: vk::SUBPASS_EXTERNAL,
+        src_stage_mask: vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT,
+        dst_access_mask: vk::AccessFlags::COLOR_ATTACHMENT_READ
+            | vk::AccessFlags::COLOR_ATTACHMENT_WRITE,
+        dst_stage_mask: vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT,
+        ..Default::default()
+    }];
+
+    let renderpass_attachments = [
+        vk::AttachmentDescription {
+            format: surface_format.format,
+            samples: vk::SampleCountFlags::TYPE_1,
+            load_op: vk::AttachmentLoadOp::CLEAR,
+            store_op: vk::AttachmentStoreOp::STORE,
+            final_layout: vk::ImageLayout::PRESENT_SRC_KHR,
+            ..Default::default()
+        },
+        vk::AttachmentDescription {
+            format: vk::Format::D16_UNORM,
+            samples: vk::SampleCountFlags::TYPE_1,
+            load_op: vk::AttachmentLoadOp::CLEAR,
+            initial_layout: vk::ImageLayout::DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
+            final_layout: vk::ImageLayout::DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
+            ..Default::default()
+        },
+    ];
+
+    let color_attachment_refs = [vk::AttachmentReference {
+        attachment: 0,
+        layout: vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL,
+    }];
+    let depth_attachment_ref = vk::AttachmentReference {
+        attachment: 1,
+        layout: vk::ImageLayout::DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
+    };
+
+    let subpass = vk::SubpassDescription::default()
+        .color_attachments(&color_attachment_refs)
+        .depth_stencil_attachment(&depth_attachment_ref)
+        .pipeline_bind_point(vk::PipelineBindPoint::GRAPHICS);
+
+    let renderpass_create_info = vk::RenderPassCreateInfo::default()
+        .attachments(&renderpass_attachments)
+        .subpasses(std::slice::from_ref(&subpass))
+        .dependencies(&dependencies);
+
+    let renderpass = unsafe {
+        device
+            .create_render_pass(&renderpass_create_info, None)
+    }
+        .unwrap();
+
+    let graphic_pipeline_info = vk::GraphicsPipelineCreateInfo::default()
+        .stages(&shader_stage_create_infos)
+        .vertex_input_state(&vertex_input_state_info)
+        .input_assembly_state(&vertex_input_assembly_state_info)
+        .viewport_state(&viewport_state_info)
+        .rasterization_state(&rasterization_info)
+        .multisample_state(&multisample_state_info)
+        .depth_stencil_state(&depth_state_info)
+        .color_blend_state(&color_blend_state)
+        .dynamic_state(&dynamic_state_info)
+        .layout(pipeline_layout)
+        .render_pass(renderpass);
+    
+
+    let graphics_pipelines = unsafe {
+        device
+            .create_graphics_pipelines(vk::PipelineCache::null(), &[graphic_pipeline_info], None)
+    }
+        .expect("Unable to create graphics pipeline");
+    
+    
     window.set_key_polling(true);
 
     while !window.should_close() {
@@ -220,7 +467,95 @@ fn main() {
                     window.set_should_close(true)
                 }
                 _ => {}
-            }        }
+            }        
+        }
+
+        let (present_index, _) = unsafe { swapchain_loader
+            .acquire_next_image(
+                swapchain,
+                u64::MAX,
+                present_complete_semaphore,
+                vk::Fence::null(),
+            )}
+            .unwrap();
+        let clear_values = [
+            vk::ClearValue {
+                color: vk::ClearColorValue {
+                    float32: [0.0, 0.0, 0.0, 0.0],
+                },
+            },
+            vk::ClearValue {
+                depth_stencil: vk::ClearDepthStencilValue {
+                    depth: 1.0,
+                    stencil: 0,
+                },
+            },
+        ];
+
+        let render_pass_begin_info = vk::RenderPassBeginInfo::default()
+            .render_pass(renderpass)
+            .framebuffer(framebuffers[present_index as usize])
+            .render_area(surface_resolution.into())
+            .clear_values(&clear_values);
+
+        record_submit_commandbuffer(
+            &device,
+            draw_command_buffer,
+            draw_commands_reuse_fence,
+            present_queue,
+            &[vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT],
+            &[present_complete_semaphore],
+            &[rendering_complete_semaphore],
+            |device, draw_command_buffer| {
+                device.cmd_begin_render_pass(
+                    draw_command_buffer,
+                    &render_pass_begin_info,
+                    vk::SubpassContents::INLINE,
+                );
+                device.cmd_bind_pipeline(
+                    draw_command_buffer,
+                    vk::PipelineBindPoint::GRAPHICS,
+                    graphic_pipeline,
+                );
+                device.cmd_set_viewport(draw_command_buffer, 0, &viewports);
+                device.cmd_set_scissor(draw_command_buffer, 0, &scissors);
+                device.cmd_bind_vertex_buffers(
+                    draw_command_buffer,
+                    0,
+                    &[vertex_input_buffer],
+                    &[0],
+                );
+                device.cmd_bind_index_buffer(
+                    draw_command_buffer,
+                    index_buffer,
+                    0,
+                    vk::IndexType::UINT32,
+                );
+                device.cmd_draw_indexed(
+                    draw_command_buffer,
+                    index_buffer_data.len() as u32,
+                    1,
+                    0,
+                    0,
+                    1,
+                );
+                // Or draw without the index buffer
+                // device.cmd_draw(draw_command_buffer, 3, 1, 0, 0);
+                device.cmd_end_render_pass(draw_command_buffer);
+            },
+        );
+        let wait_semaphors = [rendering_complete_semaphore];
+        let swapchains = [swapchain];
+        let image_indices = [present_index];
+        let present_info = vk::PresentInfoKHR::default()
+            .wait_semaphores(&wait_semaphors) // &rendering_complete_semaphore)
+            .swapchains(&swapchains)
+            .image_indices(&image_indices);
+
+        unsafe { swapchain_loader
+            .queue_present(present_queue, &present_info) }
+            .unwrap();
+        
     }
 
     // SyncraOS is the main driver and broker for Syncra. It drives the Vulkan renderer
