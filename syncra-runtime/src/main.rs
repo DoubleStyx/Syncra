c72896b92b48d677bab93dc4304a86be51a4950e
DoubleStyx <79298541+DoubleStyx@users.noreply.github.com>
2024-12-12 00:39:47 -0600

    update license, add other crates

--- syncra-runtime/src/main.rs
+++ syncra-runtime/src/main.rs
@@ -1,19 +1,3 @@
-mod window;
-
-use wgpu;
-use winit;
-use winit::application::ApplicationHandler;
-use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
-use crate::window::App;
-
 fn main() {
-    env_logger::init();
 
-    let event_loop = EventLoop::new().unwrap();
-    event_loop.set_control_flow(ControlFlow::Poll);
-
-    let mut app = App::default();
-    if let Err(e) = event_loop.run_app(&mut app) {
-        eprintln!("Event loop error: {e}");
-    }
 }
