631eff4379fd3ebc8faee2035c2dfd7a30f2f63f
DoubleStyx <79298541+DoubleStyx@users.noreply.github.com>
2024-12-12 01:14:42 -0600

    set up windowing, add ipc crate

---
+++ syncra-window/src/main.rs
@@ -0,0 +1,23 @@
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::{Window, WindowId};
+use crate::app::App;
+
+mod app;
+
+fn main() {
+    let event_loop = EventLoop::new().unwrap();
+
+    // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
+    // dispatched any events. This is ideal for games and similar applications.
+    event_loop.set_control_flow(ControlFlow::Poll);
+
+    // ControlFlow::Wait pauses the event loop if no events are available to process.
+    // This is ideal for non-game applications that only update in response to user
+    // input, and uses significantly less power/CPU time than ControlFlow::Poll.
+    event_loop.set_control_flow(ControlFlow::Wait);
+
+    let mut app = App::default();
+    event_loop.run_app(&mut app);
+}
